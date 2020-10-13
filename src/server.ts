8b7e14b4e5d45c4e78a9739a517434c0e6411f45
daniel21h <hesseldaniel21@gmail.com>
2020-10-13 20:55:16 -0300

    feat: abstraction in routes and controllers

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,12 @@
 import express from "express";
-import { getRepository } from "typeorm";
-import Orphanage from "./models/Orphanage";
-
 import "./database/connection";
+
+import routes from "./routes";
 
 const server = express();
 
 server.use(express.json());
-
-server.post("/orphanages", async (request, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = request.body;
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return response.json(orphanage);
-});
+server.use(routes);
 
 server.listen(3333, () => {
   console.log("🙃 Server running on port 3333.");
