3564984d0713c484c4932accd3c6655744f07a17
daniel21h <hesseldaniel21@gmail.com>
2020-10-13 20:44:46 -0300

    feat: add create orphanage function

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "./models/Orphanage";
 
 import "./database/connection";
 
@@ -6,8 +8,31 @@ const server = express();
 
 server.use(express.json());
 
-server.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+server.post("/orphanages", async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.json(orphanage);
 });
 
 server.listen(3333, () => {
