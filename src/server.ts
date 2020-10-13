3e9c6d3bb2f763d46e3eb9f5ef772bc1e5cb178a
daniel21h <hesseldaniel21@gmail.com>
2020-10-13 19:27:58 -0300

    feat: hello world in node.js

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const server = express();
+
+server.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
+
+server.listen(3333, () => {
+  console.log("🙃 Server running on port 3333.");
+});
