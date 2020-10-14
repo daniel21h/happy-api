8f401f6f8bf6d8aaf36bc2edff810fdbf3a51efe
daniel21h <hesseldaniel21@gmail.com>
2020-10-14 20:23:09 -0300

    feat: datas validation

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import path from "path";
+import "express-async-errors";
 
 import "./database/connection";
 
 import routes from "./routes";
+import errorHandler from "./config/errors/handler";
 
 const server = express();
 
 server.use(express.json());
 server.use(routes);
 server.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+server.use(errorHandler);
 
 server.listen(3333, () => {
   console.log("🙃 Server running on port 3333.");
