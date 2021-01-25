b2915aff3818139205908f5c31e7694447c2a2d6
daniel21h <hesseldaniel21@gmail.com>
2021-01-25 19:06:23 -0300

    feat: add images url static

--- src/views/images_view.ts
+++ src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://localhost:3333/uploads/${image.path}`,
+      url: `https://happy-server-nodejs.herokuapp.com/${image.path}`,
     };
   },
 
